d25e6c3c7bca56d3ede79df9e2d6da2f23dd0b89
Federico Collasius <fede.collasius@gmail.com>
2023-10-14 00:15:32 -0300

    Corregi generacion de datos

--- codigo/scripts/generacion_de_datos.py
+++ codigo/scripts/generacion_de_datos.py
@@ -4,26 +4,14 @@ import os
 import nltk
 from nltk.corpus import words
 
-nltk.download('words')
-
-def generar_archivo_datos(num_palabras=1000, longitud_palabra=None, idioma=True, repeticion_unica_palabra=False):
-    if idioma:
-        todas_las_palabras = words.words()
-        if longitud_palabra:
-            todas_las_palabras = filtrar_por_longitud(todas_las_palabras, longitud_palabra)
-    else:
-        todas_las_palabras = generar_palabras_aleatorias(num_palabras, longitud_palabra)
-    
-    if repeticion_unica_palabra:
-        todas_las_palabras = [todas_las_palabras[0]] * num_palabras
-
-    nombre_archivo = construir_nombre_archivo(idioma, repeticion_unica_palabra, num_palabras)
-
-    guardar_archivo(todas_las_palabras, nombre_archivo)
-    print(f"Archivo {nombre_archivo} generado")
-
 def filtrar_por_longitud(lista, longitud):
     return [palabra for palabra in lista if longitud[0] <= len(palabra) <= longitud[1]]
+
+def obtener_palabras_aleatorias_del_corpus(num_palabras, longitud_palabra=None):
+    todas_las_palabras = words.words()
+    if longitud_palabra:
+        todas_las_palabras = filtrar_por_longitud(todas_las_palabras, longitud_palabra)
+    return random.sample(todas_las_palabras, num_palabras)
 
 def generar_palabras_aleatorias(num_palabras, longitud_palabra):
     if longitud_palabra:
@@ -31,10 +19,10 @@ def generar_palabras_aleatorias(num_palabras, longitud_palabra):
     else:
         return [''.join(random.choices(string.ascii_lowercase, k=random.randint(1, 15))) for _ in range(num_palabras)]
 
-def construir_nombre_archivo(idioma, repeticion_unica_palabra, num_palabras):
+def construir_nombre_archivo(idioma, num_palabras, num_archivos, archivo_actual):
     nombre = f"datos_{'idioma' if idioma else 'aleatorio'}_"
-    if repeticion_unica_palabra:
-        nombre += "repeticion_unica_"
+    if num_archivos > 1:
+        nombre += f"parte{archivo_actual}_de_{num_archivos}_"
     nombre += f"{num_palabras}palabras.txt"
     return nombre
 
@@ -42,12 +30,21 @@ def guardar_archivo(palabras, nombre_archivo):
     directorio = "data/archivos"
     if not os.path.exists(directorio):
         os.makedirs(directorio)
-    with open(f"data/archivos/{nombre_archivo}", "w") as f:
+    with open(f"{directorio}/{nombre_archivo}", "w") as f:
         for palabra in palabras:
             f.write(palabra + '\n')
 
-
+def generar_archivo_datos(num_palabras=1000, longitud_palabra=None, idioma=True, num_archivos=1):
+    if idioma:
+        todas_las_palabras = obtener_palabras_aleatorias_del_corpus(num_palabras * num_archivos, longitud_palabra)
+    else:
+        todas_las_palabras = generar_palabras_aleatorias(num_palabras * num_archivos, longitud_palabra)
+    
+    for i in range(num_archivos):
+        palabras_actuales = todas_las_palabras[i*num_palabras:(i+1)*num_palabras]
+        nombre_archivo = construir_nombre_archivo(idioma, num_palabras, num_archivos, i+1)
+        guardar_archivo(palabras_actuales, nombre_archivo)
+        print(f"Archivo {nombre_archivo} generado")
 
 # Ejemplo 
-generar_archivo_datos(num_palabras=100, longitud_palabra=(3, 7), idioma=True)
-
+generar_archivo_datos(num_palabras=100, longitud_palabra=(3, 7), num_archivos=3)
