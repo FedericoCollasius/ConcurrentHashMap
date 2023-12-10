5c1925df233ae3797ec170408a6c7ab35d67e137
RamiSammy <69830552+RamiSammy@users.noreply.github.com>
2023-12-10 20:42:46 -0300

    Update experimento5.cpp

--- codigo/experimentacion/experimento5/experimento5.cpp
+++ codigo/experimentacion/experimento5/experimento5.cpp
@@ -50,7 +50,7 @@ double concurrencia (vector<string> palabras , int cantidadPalabras){
         auto t1 = Clock::now();
 
         HashMapConcurrente hashmap;
-        vector<string> libro = {"ejemplo.txt"};
+        vector<string> libro = {"draculaParsed.txt"};
         cargarMultiplesArchivos(hashmap, 1, libro); //Solo un thread carga el archivo? 
         vector<int> contador(cantidadPalabras , 0);
         for (int i = 0; i < cantidadPalabras; i++)
@@ -74,7 +74,7 @@ double concurrencia (vector<string> palabras , int cantidadPalabras){
 
 
 void abrirArchivo(){
-    const string nombre_archivo = "ejemplo.txt";
+    const string nombre_archivo = "draculaParsed.txt";
     ifstream archivo_entrada(nombre_archivo);
 
         if (!archivo_entrada.is_open()) {
@@ -85,7 +85,7 @@ void abrirArchivo(){
 }
 
 double contadorApariciones (vector<string> palabras , int cantidadPalabras){
-    const string nombre_archivo = "ejemplo.txt";
+    const string nombre_archivo = "draculaParsed.txt";
     double tiempo = 0;
 
     for (int j = 0; j < repes ; j++){
@@ -142,20 +142,25 @@ vector<std::string> readWordsIntoVector(const std::string& filename) {
     return words;
 }
 
-
-
 int main(int argc, char* argv[]) {
 
-    //Eliminar del libro , : mayuscula numeros
+
 
     const string OUTPUT_FILE = "resultado.txt";
 
     vector<string> palabras = readWordsIntoVector("randomWordsDracula.txt");
 
+    for (int i = 0; i < palabras.size() ; i++)
+    {
+        cout << palabras[i] << endl;
+    }
+    
+
+
     
     ofstream outFile(OUTPUT_FILE);
 
-    for (int i = 1; i < palabras.size() ; ++i) {
+    for (int i = 1; i <= palabras.size() ; ++i) {
         double resultadoSinConcurrencia = contadorApariciones(palabras , i);
         double resultadoConConcurrencia = concurrencia(palabras , i);
         string resultado = to_string(i) + " " + to_string(resultadoSinConcurrencia) + " " + to_string(resultadoConConcurrencia);
